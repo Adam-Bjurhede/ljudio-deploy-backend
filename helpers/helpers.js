6e18318c424a1a5ccf1adc10d0f91494f5820507
krispad-dev <kristofer.padoan@icloud.com>
2022-02-01 22:08:39 +0100

    val helper bug fix

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export const Bcrypt = {
   async hashPassword(password) {
@@ -18,7 +19,7 @@ export const Bcrypt = {
 };
 
 export async function generateToken(data) {
-  const token = await jwt.sign(data, 'sEcReTkEy', { expiresIn: '60d' });
+  const token = await jwt.sign(data, process.env.SECRET, { expiresIn: '60d' });
   return token;
 }
 
